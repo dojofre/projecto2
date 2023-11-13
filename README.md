68ba4103074415ce8783d0e40a7e861adb0db8ad
Domingo Jofre <dojofre@gmail.com>
2023-11-13 19:48:44 -0300

    actualizacion 131123-2

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-
-cortes
-301 - 400
-401 - 499
-501 - 599
-601 - 700
-701 - 800
-
-
     <footer class="footer1">
       <div class="wrapper--footer">
         <div class="link facebook" id="btn_facebook">
@@ -35,4 +26,108 @@ cortes
         <p class="job-text" id="f-text1">© 2023.</p>
       </div>
     </footer>
+
+
+
+//formulario contacto
+const form = document.getElementById('form');
+const username = document.getElementById('from_name');
+const email = document.getElementById('email');
+const fono = document.getElementById('phone');
+const contacto = document.getElementById('contac');
+const mensaje = document.getElementById('message');
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    // .addEventListener('submit', function (event) {
+    //     event.preventDefault();//se esta previniendo que el formulario se envie por defecto
+
+    //     btn.value = 'Enviando...';
+
+    //     const serviceID = 'default_service';
+    //     const templateID = 'template_xhtkiyn';
+
+    //     emailjs.sendForm(serviceID, templateID, this)
+    //         .then(() => {
+    //             btn.value = 'Correo enviado';
+    //             alert('Información enviada');
+    //         }, (err) => {
+    //             btn.value = 'Correo enviado';
+    //             alert(JSON.stringify(err));
+    //         });
+    // });
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+    //sendForm();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const fonoValue = fono.value.trim();
+    const contactoValue = contacto.value.trim();
+    const mensajeValue = mensaje.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Su nombre es requerido');
+    } else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Su correo es requerido');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Ingrese una dirección de correo valida');
+    } else {
+        setSuccess(email);
+    }
+
+    if (fonoValue === '') {
+        setError(fono, 'Ingrese su telefono');
+    } else {
+        setSuccess(fono);
+    }
+
+    if (contactoValue === '') {
+        setError(contacto, '¿Diganos, desde donde nos contacta?');
+    } else {
+        setSuccess(contacto);
+    }
+
+    if (mensajeValue === '') {
+        setError(mensaje, '¿Dejanos un mensaje para conocer tus inquietudes?');
+    } else {
+        setSuccess(mensaje);
+    }
+
+};
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,102 +1,124 @@
 //formulario contacto
-const form = document.getElementById('form');
-const username = document.getElementById('from_name');
-const email = document.getElementById('email');
-const fono = document.getElementById('phone');
-const contacto = document.getElementById('contac');
-const mensaje = document.getElementById('message');
+document.addEventListener('DOMContentLoaded', function () {
+    //const formulario = document.getElementById('form');
 
+    const form = document.getElementById('form');
+    const username = document.getElementById('from_name');
+    const email = document.getElementById('email');
+    const fono = document.getElementById('phone');
+    const contacto = document.getElementById('contac');
+    const mensaje = document.getElementById('message');
 
-const btn = document.getElementById('button');
+    const btn = document.getElementById('button');
 
-document.getElementById('form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();//se esta previniendo que el formulario se envie por defecto
-
-        validateInputs();
-
-        //    btn.value = 'Enviando...';
-
-        //    const serviceID = 'default_service';
-        //    const templateID = 'template_xhtkiyn';
-
-        //    emailjs.sendForm(serviceID, templateID, this)
-        //     .then(() => {
-        //       btn.value = 'Correo enviado';
-        //       alert('Mensaje Enviado');
-        //     }, (err) => {
-        //       btn.value = 'Correo enviado';
-        //       alert(JSON.stringify(err));
-        //     });
-    });
-
-    
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-    
-        validateInputs();
-    });
-    
     const setError = (element, message) => {
         const inputControl = element.parentElement;
         const errorDisplay = inputControl.querySelector('.error');
-    
+
         errorDisplay.innerText = message;
         inputControl.classList.add('error');
         inputControl.classList.remove('success')
     }
-    
+
     const setSuccess = element => {
         const inputControl = element.parentElement;
         const errorDisplay = inputControl.querySelector('.error');
-    
+
         errorDisplay.innerText = '';
         inputControl.classList.add('success');
         inputControl.classList.remove('error');
+
     };
-    
+
     const isValidEmail = email => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
-    
-    const validateInputs = () => {
-        const usernameValue = username.value.trim();
-        const emailValue = email.value.trim();
-        const fonoValue = fono.value.trim();
-        const contactoValue = contacto.value.trim();
-        const mensajeValue = mensaje.value.trim();
-    
-        if(usernameValue === '') {
+
+    var validarNombre = function (e) {
+        if (username.value.length == 0) {
             setError(username, 'Su nombre es requerido');
+            return false;
         } else {
             setSuccess(username);
+            return true;
         }
-    
-        if(emailValue === '') {
+    };
+
+    var validarCorreo = function () {
+        if (email.value.length == 0) {
             setError(email, 'Su correo es requerido');
-        } else if (!isValidEmail(emailValue)) {
-            setError(email, 'Ingrese una dirección de correo valida');
+            return false;
+            //} else if (!isValidEmail(email)) {
+            //setError(email, 'Ingrese una dirección de correo valida');
         } else {
             setSuccess(email);
+            return true;
         }
-    
-        if(fonoValue === '') {
+    };
+
+    var validarTelefono = function () {
+        if (fono.value == "") {
             setError(fono, 'Ingrese su telefono');
+            return false;
         } else {
             setSuccess(fono);
+            return true;
         }
-    
-        if(contactoValue === '') {
+    };
+
+    var validarContacto = function () {
+        if (contacto.value == "") {
             setError(contacto, '¿Diganos, desde donde nos contacta?');
+            return false;
         } else {
             setSuccess(contacto);
+            return true;
         }
+    };
 
-        if(mensajeValue === '') {
+    var validarMensaje = function () {
+        if (mensaje.value.length == 0) {
             setError(mensaje, '¿Dejanos un mensaje para conocer tus inquietudes?');
+            return false;
         } else {
             setSuccess(mensaje);
+            return true;
         }
-    
     };
+
+    var validar = function () {
+        return validarNombre() &&
+            validarCorreo() &&
+            validarTelefono() &&
+            validarContacto() &&
+            validarMensaje();
+    };
+
+
+    form.addEventListener('submit', function (event) {
+    event.preventDefault();//se esta previniendo que el formulario se envie por defecto
+
+    if (validar() == true) { 
+
+        btn.value = 'Enviando...';
+
+            const serviceID = 'default_service';
+            const templateID = 'template_xhtkiyn';
+
+            emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    alert('Gracias por contactarte con nosotros. En breve nos estaremos comunicando');
+                    this.reset();
+                    location.href = location.href;
+                }, (err) => {
+                    btn.value = 'Información enviada';
+                    alert(JSON.stringify(err));
+                });
+
+     }
+
+});
+
+
+});
